72fcd82f992784f9b82d606aa7c78d404848f653
TemirovaAzima <temirovaazima9@gmail.com>
2025-12-11 12:16:44 +0100

    (task1 ppj18)Thermostat class added

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,11 @@
 public static void main(String[] args) {
+        Thermostat thermostat = new Thermostat(0,25);
+        thermostat.turnOn();
+        System.out.println(thermostat.isOn());
+        thermostat.setTargetTemperature(90);
+        System.out.println(thermostat.status());
+        thermostat.turnOff();
+        System.out.println(thermostat.status());
+
 
 }
