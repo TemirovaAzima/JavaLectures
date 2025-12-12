71793fdcca06d012afc3d9fb8f3855a5a1ad13db
TemirovaAzima <temirovaazima9@gmail.com>
2025-12-12 16:52:16 +0100

    (Inheritance) Pixel,Point classes end

---
+++ Inheritance/src/Pixel.java
@@ -0,0 +1,33 @@
+import java.awt.Color;
+
+public class Pixel extends Point{
+    private Color color;
+
+    public Pixel(int x, int y, Color color){
+        super(x,y);
+        this.color = color;
+    }
+
+    public Pixel(int x, int y){
+        this(x,y,Color.BLACK);
+    }
+    public Pixel(Color color){
+        this(0,0,color);
+    }
+
+    public Pixel(){
+        this(0,0,Color.BLACK);
+    }
+
+    // not inherited
+    public Color getColor(){
+        return color;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + "r)=" + color.getRed() +
+                ",g=" + color.getGreen() + ",b=" +
+                color.getBlue() + ")";
+    }
+}
