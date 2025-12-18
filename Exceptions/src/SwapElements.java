6851c76e8fd143cd5fa5988579e244da2de8266c
TemirovaAzima <temirovaazima9@gmail.com>
2025-12-18 18:06:00 +0100

    PPJ test swaps the firsthalf with the second half, preserving the internal order of each half.

--- Exceptions/src/SwapElements.java
+++ Exceptions/src/SwapElements.java
@@ -5,18 +5,19 @@ public class SwapElements {
 
     public static void swElems(int[] arr){
 
-//        for(int i = arr.length/2; i <arr.length/2; i++){
+//        for(int i = arr.length/2,j = arr.length-1; i<j; i++,j--){
 //            int tmp = arr[i];
-//            arr[i] = arr[arr.length-1-i];
-//            arr[arr.length-1 -i] = tmp;
-//
+//            arr[i] = arr[j];
+//            arr[j] = tmp;
 //        }
-        for(int i = arr.length/2,j = arr.length-1; i<j; i++,j--){
+//
+//        System.out.println(Arrays.toString(arr));
+
+        for(int i =0 , j = arr.length/2; i < arr.length/2; i++,j++){
             int tmp = arr[i];
             arr[i] = arr[j];
             arr[j] = tmp;
         }
-
         System.out.println(Arrays.toString(arr));
     }
 }
